f098cd2d904df6d45ab4898e6892410c1ef16d21
kareemshowno <65797037+kareemshowno@users.noreply.github.com>
2020-09-09 14:46:47 +0300

    Updates

---
+++ precache-manifest.04aaaf7ebbaf9fcac27a440c871867b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a98636f6e9023b65e6b89559900233",
+    "url": "/Smart-Brain-App/index.html"
+  },
+  {
+    "revision": "92460636efd8da08094a",
+    "url": "/Smart-Brain-App/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "39e30e8a4a8f0d6d2dd9",
+    "url": "/Smart-Brain-App/static/css/main.522be2c6.chunk.css"
+  },
+  {
+    "revision": "92460636efd8da08094a",
+    "url": "/Smart-Brain-App/static/js/2.18012286.chunk.js"
+  },
+  {
+    "revision": "a73511fdd0e79fea52c5f42ad1574633",
+    "url": "/Smart-Brain-App/static/js/2.18012286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39e30e8a4a8f0d6d2dd9",
+    "url": "/Smart-Brain-App/static/js/main.07a7a9cd.chunk.js"
+  },
+  {
+    "revision": "52eaa638056d38bc0a25",
+    "url": "/Smart-Brain-App/static/js/runtime-main.f5a76041.js"
+  }
+]);
